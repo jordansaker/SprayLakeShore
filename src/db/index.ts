96e01a764a4892a7a964a9374c824e9cfd02cc77
jordansaker <jordan.saker@ymail.com>
2023-12-27 14:29:46 +1000

    created migrations sql file

---
+++ src/db/index.ts
@@ -0,0 +1,33 @@
+import path from 'path'
+import pg from 'pg'
+import { migrate } from 'postgres-migrations'
+
+let config = {
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, 
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  max: Number(process.env.DB_POOL_SIZE),
+  idleTimeoutMillis: Number(process.env.DB_POOL_CLIENT_IDLE_TIMEOUT),
+  connectionTimeoutMillis: Number(
+    process.env.DB_POOL_CLIENT_CONNECTION_TIMEOUT
+  )
+}
+
+const pool = new pg.Pool(config)
+
+const db = {
+  runMigrations: async function (): Promise<void> {
+    const client = await pool.connect()
+    try {
+      await migrate({ client }, path.resolve(__dirname, 'migrations/sql'))
+    } catch (err) {
+      console.error('migration failed', err)
+    } finally {
+      client.release()
+    }
+  }
+}
+
+export default db
---
+++ src/db/migrations/sql/1_create-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS
+    users(
+      username varchar(128) PRIMARY KEY NOT NULL,
+      email varchar(128) NOT NULL,
+      user_role varchar(128) NOT NULL,
+      password varcahr(128) NOT NULL
+    );
