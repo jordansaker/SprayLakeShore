369c318215546350cc38575783cf23a2f209cca6
jordansaker <jordan.saker@ymail.com>
2024-01-04 10:53:06 +1000

    added spray_equipment insert sql script

---
+++ src/db/migrations/sql/4_insert-spray-equipments.sql
@@ -0,0 +1,4 @@
+INSERT into spray_equipments(spray_equipment) values ('S2');
+INSERT into spray_equipments(spray_equipment) values ('S3');
+INSERT into spray_equipments(spray_equipment) values ('S9');
+INSERT into spray_equipments(spray_equipment) values ('S10');
