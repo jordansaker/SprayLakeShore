9f74887b8be0196301b6a91cbd971de94d2f050a
jordansaker <jordan.saker@ymail.com>
2024-01-04 13:28:36 +1000

    using relative path for importing models

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import { User, UserRole } from "models/user"
+import { User, UserRole } from "../src/models/user"
 
 const baseURL: string = 'http://localhost:3000'
 // Objefcts used in tests
