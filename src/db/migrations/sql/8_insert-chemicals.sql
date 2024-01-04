095fcf395da7c056a6cd244848b18edebd87147a
jordansaker <jordan.saker@ymail.com>
2024-01-04 11:17:49 +1000

    added 2 chemicals to chemicals migration script, updated wind direction script with semicolons

---
+++ src/db/migrations/sql/8_insert-chemicals.sql
@@ -0,0 +1,2 @@
+INSERT into chemicals(chemical_name, targeted_pest, chemical_rate) values ('Serenade Opti', 'Anthracnose', 150);
+INSERT into chemicals(chemical_name, targeted_pest, chemical_rate) values ('Transform', 'FSB', 40);
